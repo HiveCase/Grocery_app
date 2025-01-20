25176c0f4b5ae550dc7056b1e50bb3bfd1d87818
HiveCase <23f1003132@ds.study.iitm.ac.in>
2025-01-20 13:24:15 +0000

    models added

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,23 @@ from flask import Flask
 
 
 app = Flask(__name__)
-
 @app.route('/')
 def home():
-    return 'Hello World'
+    return '<h3>Hello World</h3>'
+
+@app.route('/api/')
+def api():
+    return {"message":"Route based API"}
+
+class Home():
+    def get(self):
+        return {"message":"Class based API"}
+@app.route('/home/')
+def api():
+    return 
+
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
